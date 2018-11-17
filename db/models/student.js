92b3d87acebc9907bc15b9a34fb4100460356507
MCClarke <mcarlotclarke@gmail.com>
2018-11-17 11:45:22 -0500

    Sequelize study cycle 1

--- db/models/student.js
+++ db/models/student.js
@@ -3,6 +3,27 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
+});
+
+Student.beforeCreate((newStudent) => {
+  newStudent.firstName = newStudent.firstName.slice(0, 1).toUpperCase() + newStudent.firstName.slice(1);
+  newStudent.lastName = newStudent.lastName.slice(0, 1).toUpperCase() + newStudent.lastName.slice(1);
+});
 
 module.exports = Student;
